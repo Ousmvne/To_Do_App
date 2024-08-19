fc49e1ed4b4e43284fb6c7492f5e2da71f247651
Ousmvne <157376708+Ousmvne@users.noreply.github.com>
2024-08-19 11:14:32 +0000

    Inclusion de difficulty et category dans models.py

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -2,9 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
+    DIFFICULTY_CHOICES = [
+        ('Facile', 'Facile'),
+        ('Moyen', 'Moyen'),
+        ('Difficile', 'Difficile'),
+    ]
+
+    CATEGORY_CHOICES = [
+        ('Personal', 'Personal'),
+        ('Leisure', 'Leisure'),
+        ('Work', 'Work'),
+    ]
+
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES, default='Moyen')
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default='Personal')
 
-    def _str_(self):
+    def __str__(self):
         return self.title
